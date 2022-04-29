4fc95869c781a84d9defede173e7ccf7d6a9703d
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-04-29 15:04:00 -0400

    made code dyer

--- getRestaurants.js
+++ getRestaurants.js
@@ -1,16 +1,4 @@
-import admin from "firebase-admin"; // imports firebase library
-
-import serviceAccount from './credentials.js'; // import our credentials to connect to firebase
-
-admin.initializeApp({ //connects to our firebase project 
-  credential: admin.credential.cert(serviceAccount) // creating a certificate from our credentials
-});
-
-// Now we are connect to our Firebase project & related services
-
-const db = admin.firestore(); // creates a short to access the Firestore database
-
-const restaurantsCol = db.collection('restaurants');
+import { restaurantsCol } from './connectdb.js'
 
 // restaurantsCol.get() // This is saying get all the restaurants
 //     .then(snapshot => {
